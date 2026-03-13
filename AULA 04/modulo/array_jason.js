fd3fcc75367e9d072d5f75496a015a8a8504fab3
DIEGO DE PADUA BEZERRA DE LEMOS <sn1094808@cfp127aluno.local>
2026-03-13 11:17:19 -0300

    aula de back (array e json)

--- AULA 04/modulo/array_jason.js
+++ AULA 04/modulo/array_jason.js
@@ -36,7 +36,7 @@
  */
 
 //Formas de criar um ARRAY
-const listaDeNomes = ['José', 'Maria', 'João', 'André', 'Alex']
+const listaDeNomes = ['José', 'Maria', 'João', 'André', 'Alex', 'Carlos', 'Ana', 'Bruna', 'Jake']
 const listaDeClientes = []
 const listaDeFornecedores = []
 
@@ -113,13 +113,192 @@ const manipularDados = function(){
 
     console.log(listaDeClientes)
 
-    //Permite adicionar novos valores no array sempre no final da lista
+    //Permite adicionar novos valores no array sempre no FINAL da lista
     listaDeFornecedores.push('Luiz da Silva')
     listaDeFornecedores.push('Zezinho da Silva')
     listaDeFornecedores.push('Huguinho da Silva')
+    listaDeFornecedores.push('Huguinho da Silva','André da Silva', 'Carlos da Silva')
 
+    //Permite adicionar novos elementos no array sempre no INICIO da lista
+    listaDeFornecedores.unshift('Ana Carolina')
     console.log(listaDeFornecedores)
+
+    //Permite remover elementos do FINAL da lista
+    listaDeFornecedores.pop()
+    console.table(listaDeFornecedores)
+
+    //Permite remover elementos do INICIO da lista
+    listaDeFornecedores.shift()
+    console.table(listaDeFornecedores)
+
+    //Splice()--> Permite remover um elemento baseado no indice da lista 
+        // splice(indice, quantidade de elementos)
+    listaDeFornecedores.splice(2,1)
+    console.table(listaDeFornecedores)
+
+    //Splice()--> Permite adicionar um novo elemento em um determinado lugar do array(indice)
+                            //indice, 0--> siginifica que não será removido ninguém, Novo conteúdo   
+    listaDeFornecedores.slice(2,0,'Carlos da Silva')
+    console.table(listaDeFornecedores)
+
+    
 }
 
-manipularDados()
+const removerItem = function(nome){
+
+    //Retorna o indice de um elemento fazendo a busca pelo conteúdo
+    //Se o indexOf não encontrar o conteúdo ele devolve -1
+    let indice = listaDeNomes.indexOf(nome)
+    listaDeNomes.splice(indice,1)
+    if(indice != -1){
+        listaDeNomes.splice(indice,1)
+        return true
+    }else{
+        return false
+    }
+
+    //Retorna o indice de um elemento fazendo a busca pelo valor
+    //for(indice in listaDeNomes)
+        //if(listaDeNomes[indice] == nome){
+            //listaDeNomes.splice(indice,1)
+        //}
+
+}
+
+const verificarItem = function(nome){
+    //verifica a existencia de um conteúdo dentro de uma lista (true/false)
+    let resposta = listaDeNomes.includes()
+
+}
+
+const quantidadeItens = function(nome){
+    let cont =  0 
+    listaDeNomes.forEach(function(item){
+        if(String(item).toUpperCase() == String(nome).toLocaleUpperCase())
+            cont +=1
+
+    })
+}
+
+const criandoDadosJSON = function(){
+
+    let aluno = {'nome': 'José',
+                 'ra': 123456,
+                 'telefone': '97575752343',
+                 'email': 'jose@gmail.com'}
+
+
+    //exibindo o objeto JSON completo             
+    console.log(aluno)
+    console.table(aluno)
+    
+    //exibindo apenas um atributo do JSON
+    console.log(aluno.nome)
+
+    //Adiciona um novo atributo no JSON
+    aluno.sexo = 'Masculino'
+    console.log(aluno)
+
+    //deletar um atributo no JSON
+    delete aluno.telefone
+    console.log(aluno)
+
+}
+
+const cadastroDeProdutos = function(){
+
+    let cores = [
+        {'id': 1, 'cor': 'Branco'},    //indice 0
+        {'id': 2, 'cor': 'Preto'},     //indice 1
+        {'id': 3, 'cor': 'Azul'},      //indice 2
+        {'id': 4, 'cor': 'Rosa'},      //indice 3
+        {'id': 5, 'cor': 'Cinza'}      //indice 4
+    ]
+
+    let marcas = [
+        {'id': 1, 'marca': 'LG',           'telefone': '123456789', 'email': 'lg@lg.com.br'},       //indice 0
+        {'id': 2, 'marca': 'DELL',         'telefone': '123456239', 'email': 'dell@dell.com.br'},    //indice 1
+        {'id': 3, 'marca': 'LENOVO',       'telefone': '123456569', 'email': 'lenovo@lenovo.com.br'},  //indice 2
+        {'id': 4, 'marca': 'APPLE',        'telefone': '123456779', 'email': 'apple@apple.com.br'},   //indice 3
+        {'id': 5, 'marca': 'RAYZER',       'telefone': '123456899', 'email': 'rayzer@rayzer.com.br'},  //indice 4
+        {'id': 6, 'marca': 'LOGITECH',     'telefone': '123453429', 'email': 'logitech@logitch.com.br'},  //indice 5
+        {'id': 7, 'marca': 'MULTILASER',   'telefone': '123456129', 'email': 'multilaser@multilaser.com.br'}   //indice 6
+    ]
+
+    let produtos = [
+
+        {   'id': 1,
+            'nome': 'Monitor',
+            'descricao': '27 polegadas',
+            'marca': marcas[1].marca,
+            'quantidade': 20,
+            'cor':[
+                cores[4],
+                cores[1] 
+            ],
+            'valor': 800.50
+        },
+        {
+            'id': 2,
+            'nome': 'Teclado',
+            'descricao': 'Teclado mecânico RGB',
+            'marca': marcas[5].marca,
+            'quantidade': 200,
+            'cor': cores,
+            'valor': 150
+        },
+        {
+            'id': 3,
+            'nome': 'Mouse',
+            'descricao': 'Mouse sem fio',
+            'marca':[
+                marcas[0].marca,
+                marcas[1].marca,
+                marcas[5].marca
+
+            ],
+            'quantidade': 500,
+            'cor':[
+                cores[0],
+                cores[1],
+                cores[4]
+            ],
+            'valor': 80
+
+        }
+    ]
+
+
+
+    //console.log(cores)
+    //console.table(cores)
+
+    //console.log(cores[2].nome)
+
+    //console.log(produtos)
+    //console.log(produtos[0].cor)
+    //console.log(produtos[0].cor[1].cor)
+
+    console.table(produtos)
+
+    //produtos[0].cor.forEach(function(nomeCor){
+    //    console.log('A cor do produto é: '+ nomeCor.cor)
+    //})
+}
+
+//manipularDados()
 //exibirDados()
+//console.table(listaDeNomes)
+//removerItem('Maria')
+//console.log(removerItem('Bruna'))
+//console.table(listaDeNomes)
+//let resposta = removerItem('jsjdjd')
+//if(resposta)
+    //console.log('Item removido')
+
+
+
+
+//console.log(quantidadeItens('Zé'))
+//criandoDadosJSON()
+cadastroDeProdutos()
